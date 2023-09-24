1829c85a7d33784959c93365d667e182c630f7cb
Kevin Yu <kevinyu567@gmail.com>
2023-09-23 23:03:22 -0400

    Fix logic errors, publish PID on IMU read
    Publishing periodically in a separate loop would probably be bad for I and D calculations if the two loops are not synced.

--- sensors/src/imu_angle_control.py
+++ sensors/src/imu_angle_control.py
@@ -1,4 +1,6 @@
 #! /usr/bin/python3
+import time
+
 import rclpy
 from rclpy.node import Node
 
@@ -6,36 +8,33 @@ from shared_msgs.msg import ImuMsg, ImuVelocityCommand
 
 
 class ImuAngleControlNode(Node):
-    angle = [0.0, 0.0, 0.0]  # Tuple of [pitch, roll, yaw]
-    prev_angle = [0.0, 0.0, 0.0]
-    target_angle = [0.0, 0.0, 0.0]  # TODO: receiving this
-
     kp = [0.0, 0.0, 0.0]  # TODO
     ki = [0.0, 0.0, 0.0]
     kd = [0.0, 0.0, 0.0]
-    istate = [0.0, 0.0, 0.0]
 
     def __init__(self):
         super().__init__('imu_angle_control')
 
-        self.sub = self.create_subscription(ImuMsg, 'imu', self._com_update, 1)
+        self.sub = self.create_subscription(ImuMsg, 'imu', self.imu_reading, 1)
         self.pub = self.create_publisher(ImuVelocityCommand, 'imu_vel_command', 10)
 
-        self.period = 0.02
-        self.timer = self.create_timer(self.period, self.periodic)
+        self.prev_angle = [0.0, 0.0, 0.0]  # Tuple of [pitch, roll, yaw]
+        self.prev_ts = time.time()
+
+        self.target_angle = [0.0, 0.0, 0.0]  # TODO: receiving this
+        self.istate = [0.0, 0.0, 0.0]
 
     def imu_reading(self, msg):
-        self.angle = msg.gyro
+        ts = time.time()
 
-    def periodic(self):
-        msg = ImuVelocityCommand()
+        out_msg = ImuVelocityCommand()
         output = [0.0, 0.0, 0.0]
 
         # PID on each of [pitch, roll, yaw], writing outputs to `self.output`
-        for i in range(len(self.angle)):
-            error = self.target_angle[i] - self.angle[i]
+        for i in range(len(msg.gyro)):
+            error = self.target_angle[i] - msg.gyro[i]
             self.istate[i] += error * self.ki[i]
-            deriv = (self.angle[i] - self.prev_angle[i]) / self.period
+            deriv = (msg.gyro[i] - self.prev_angle[i]) / (ts - self.prev_ts)
 
             p_out = self.kp[i] * error
             i_out = self.istate[i]
@@ -43,7 +42,11 @@ class ImuAngleControlNode(Node):
 
             output[i] = p_out + i_out + d_out
 
-        msg.angular = output
+        out_msg.angular = output
+        self.pub.publish(out_msg)
+
+        self.prev_angle = msg.gyro
+        self.prev_ts = ts
 
 
 def main(args=None):

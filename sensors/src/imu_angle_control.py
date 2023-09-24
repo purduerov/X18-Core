25f7f59c9d8e13952465668609e41cb69a34d528
Kevin Yu <kevinyu567@gmail.com>
2023-09-24 15:23:42 -0400

    Fix kD to use derivative of error

--- sensors/src/imu_angle_control.py
+++ sensors/src/imu_angle_control.py
@@ -18,7 +18,7 @@ class ImuAngleControlNode(Node):
         self.sub = self.create_subscription(ImuMsg, 'imu', self.imu_reading, 1)
         self.pub = self.create_publisher(ImuVelocityCommand, 'imu_vel_command', 10)
 
-        self.prev_angle = [0.0, 0.0, 0.0]  # Tuple of [pitch, roll, yaw]
+        self.prev_error = [0.0, 0.0, 0.0]  # Tuple of [pitch, roll, yaw]
         self.prev_ts = time.time()
 
         self.target_angle = [0.0, 0.0, 0.0]  # TODO: receiving this
@@ -30,22 +30,24 @@ class ImuAngleControlNode(Node):
         out_msg = ImuVelocityCommand()
         output = [0.0, 0.0, 0.0]
 
-        # PID on each of [pitch, roll, yaw], writing outputs to `self.output`
-        for i in range(len(msg.gyro)):
-            error = self.target_angle[i] - msg.gyro[i]
+        # PID on each of [pitch, roll, yaw], writing outputs to `self.output`.
+        # Algorithm adopted from https://docs.revrobotics.com/sparkmax/operating-modes/closed-loop-control
+        # (but without `kF` and `izone`).
+        for i, angle in enumerate(msg.gyro):
+            error = self.target_angle[i] - angle
             self.istate[i] += error * self.ki[i]
-            deriv = (msg.gyro[i] - self.prev_angle[i]) / (ts - self.prev_ts)
+            deriv = (error - self.prev_error[i]) / (ts - self.prev_ts)
 
             p_out = self.kp[i] * error
             i_out = self.istate[i]
             d_out = self.kd[i] * deriv
 
+            self.prev_error[i] = error
             output[i] = p_out + i_out + d_out
 
         out_msg.angular = output
         self.pub.publish(out_msg)
 
-        self.prev_angle = msg.gyro
         self.prev_ts = ts
 
 

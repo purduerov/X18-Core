6deea1edecfa8861643467699caca2ff4da78726
XavierJCallait <xaviercallait@gmail.com>
2024-04-03 20:02:52 -0400

    Updated MY_SPI.py by adding comments and improving readability

--- thrust_control/src/MY_SPI.py
+++ thrust_control/src/MY_SPI.py
@@ -9,8 +9,12 @@ import RPi.GPIO as GPIO
 
 from shared_msgs.msg import FinalThrustMsg
 
+def invert_thrust(thrust_value):
+    return (255 - thrust_value - 1)
+
 class ThrustToSPINode(Node):
-    ZERO_THRUST = [127, 127, 127, 127, 127, 127, 127, 127]  # power of thrusters --> 127 is neutral
+    # initialize Class Variables
+    ZERO_THRUST = [127, 127, 127, 127, 127, 127, 127, 127]  # 127 is neutral
     FULL_THRUST_CONTROL = 2
     identifier = 0
     blocked = False
@@ -18,36 +22,57 @@ class ThrustToSPINode(Node):
     def __init__(self, bus=0, device=1, mode=0, speed=50000, bits_per_word=8):
         super().__init__('thrust_to_spi')
 
+        # initialize logger
         logger = self.get_logger().info("INITIALIZED")
 
+        # initialize SPI 
         self.spi = SpiDev(bus, device)
         self.spi.mode = mode
         self.spi.max_speed_hz = speed
         self.spi.bits_per_word = bits_per_word
         
-        self.thrusters = [127] * 8
+        # initialize values
+        self.thrusters = self.ZERO_THRUST
+        self.pin = 8
 
+        # initialize pull-down for chip select
+        GPIO.setwarnings(False)
         GPIO.setmode(GPIO.BCM)
-        GPIO.setup(24, GPIO.OUT, initial=GPIO.HIGH)
+        GPIO.setup(self.pin, GPIO.OUT, initial=GPIO.HIGH)
 
         # Subscribe to final_thrust and start callback function
         self.sub = self.create_subscription(
-            FinalThrustMsg, # updated 50 times per second, called each time regardless of no change
-            'final_thrust',
-            self.thrust_received,
+            FinalThrustMsg, # message, updated 50 times per second regardless of change
+            'final_thrust', # topic
+            self.thrust_received, # callback function
             10
         )
         
         return
 
+    # map thrusters to correct position and invert 
+    def thrust_map(self, thrusters):
+        mapped_thrusters = self.ZERO_THRUST
+        mapped_thrusters[0] = invert_thrust(thrusters[6])
+        mapped_thrusters[1] = invert_thrust(thrusters[2])
+        mapped_thrusters[2] = thrusters[5]
+        mapped_thrusters[3] = thrusters[1]
+        mapped_thrusters[4] = invert_thrust(thrusters[7])
+        mapped_thrusters[5] = invert_thrust(thrusters[3])
+        mapped_thrusters[6] = thrusters[0]
+        mapped_thrusters[7] = thrusters[4]
+        return mapped_thrusters
+
+    # thrust callback function
     def thrust_received(self, msg):
         print("THRUSTER RECEIVED")
-        self.thrusters = msg.thrusters
+        self.thrusters = self.thrust_map(msg.thrusters)
         self.message_received()
         return
 
-    def message_received(self):  # called in subscription object initialization
-        if (not self.blocked):    
+    # process a received message from subscription
+    def message_received(self):
+        if (not self.blocked):
             self.type = self.FULL_THRUST_CONTROL
             self.set_message_id()
             response = self.transfer(self.format_message())
@@ -68,14 +93,14 @@ class ThrustToSPINode(Node):
         byte1 = (num >> 8) & 0xFF
         return [byte1, byte2]
    
+    # swap bytes in a 2-byte list
     def swap_bytes(self, bytes_list):
         bytes_list = [bytes_list[1], bytes_list[0]]
         return bytes_list
 
     def format_message(self):
         split_id = self.split_bytes(self.identifier)
-        type_list = [self.type]
-        message = list(type_list) + list(split_id) + list(self.thrusters)
+        message = list(self.type) + list(split_id) + list(self.thrusters)
         self.compute_crc(message)
         split_crc = self.split_bytes(self.crc)
         split_crc = self.swap_bytes(split_crc)
@@ -83,40 +108,40 @@ class ThrustToSPINode(Node):
         formatted = bytearray(message)
         return formatted
 
+    '''
+    message: 11 32-bit values
+    input data format: words
+    POLY = 0x4C11DB7
+    '''
     def compute_crc(self, message):
         message_list = []
         for item in message:
             message_list.extend([0x00, 0x00, 0x00, item])
         self.crc = Crc32Mpeg2.calc(message_list)
         return
-    '''
-    message: 11 32-bit values
-    input data format: words
-    POLY = 0x4C11DB7
-    '''
     
     def transfer(self, message):
         print("MASTER: ", list(message))
         response = ()
         self.last_message = message
         if (not self.blocked):
-            GPIO.output(24, GPIO.LOW)
-            response = self.spi.xfer3(message)
-            GPIO.output(24, GPIO.HIGH)
+            GPIO.output(self.pin, GPIO.LOW)
+            response = list(self.spi.xfer3(bytearray(message)))
+            GPIO.output(self.pin, GPIO.HIGH)
         return response
     
     def response_handler(self, response):
-        print("1st SLAVE: ", response)
         if (not self.blocked):
             time.sleep(0.0001)
             message = [0] * 13
-            GPIO.output(24, GPIO.LOW)
-            response = self.spi.xfer3(bytearray(message))
-            GPIO.output(24, GPIO.HIGH)
-            print("2nd SLAVE: ", response)
+            GPIO.output(self.pin, GPIO.LOW)
+            response = list(self.spi.xfer3(bytearray(message)))
+            GPIO.output(self.pin, GPIO.HIGH)
+            print("SLAVE: ", response)
         return 
 
-    def handler(self):  # called when ctrl-C interrupt is detected
+    # error and interrupt handler 
+    def handler(self):
         print('trl-C detected')
         self.blocked = True
         self.type = self.FULL_THRUST_CONTROL
@@ -124,23 +149,35 @@ class ThrustToSPINode(Node):
         self.thrusters = self.ZERO_THRUST
         message = self.format_message()
         print("MASTER: ", list(message))
+        GPIO.output(self.pin, GPIO.LOW)
         self.spi.xfer3(message)
+        GPIO.output(self.pin, GPIO.HIGH)
         self.spi.close()
         GPIO.cleanup()
         print('Closed')
         exit(1)
 
 def main(args=None):
+    # initialize node and ROS
     rclpy.init(args=args)
     node = ThrustToSPINode()
 
+    # activate reset pin
+    GPIO.setup(23, GPIO.OUT, initial=GPIO.HIGH)
+    GPIO.output(23, GPIO.LOW)
+    time.sleep(2)
+    GPIO.output(23, GPIO.HIGH)
+
+    # run node
     try:
         rclpy.spin(node)
     except KeyboardInterrupt:
         node.handler()
-
+    
+    # cleanup
     node.destroy_node()
     rclpy.shutdown()
+    GPIO.cleanup()
 
 if __name__ == "__main__":
     main()

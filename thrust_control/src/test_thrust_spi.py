2900c401d40f8f01f4b9cff014fa3e938f10ad34
Xavier Callait <xcallait@purdue.edu>
2024-04-06 19:51:33 +0000

    Fixing testing errors

--- thrust_control/src/test_thrust_spi.py
+++ thrust_control/src/test_thrust_spi.py
@@ -5,20 +5,16 @@ from rclpy.node import Node
 
 from shared_msgs.msg import FinalThrustMsg, SPITestMsg
 
-class CommandPublisher(Node):
-    def __init__(self):
-        super().__init__('command_publisher')
-        self.command_publisher = self.create_publisher(SPITestMsg, 'SPI_command', 10)
-        logger = self.get_logger().info("PUBLISHER INITIALIZED")
-
-class CommandSubscribe(Node):
+class SPITest(Node):
     def __init__(self):
         super().__init__('test_thrust_spi')
+        self.command_publisher = self.create_publisher(SPITestMsg, 'SPI_command', 10)
         self.spi_sub = self.create_subscription(SPITestMsg, 'SPI_command', self.callback, 10)
         self.thrust_pub = self.create_publisher(FinalThrustMsg, 'final_thrust', 10)
-        logger = self.get_logger().info("SUBSCRIBER INITIALIZED")
+        logger = self.get_logger().info("TEST INITIALIZATION")
 
     def callback(self, msg):
+        print("CALLBACK")
         cmd_type = msg.type
         thrust = msg.thrust
         interval = msg.interval
@@ -28,32 +24,34 @@ class CommandSubscribe(Node):
             self.run_seq(thrust, interval)
 
     def run_all(self, thrust):
+        print("RUNNING ALL")
         tcm = FinalThrustMsg()
-        tcm.thrusters = bytearray([thrust] * 8)
+        thrusters = [thrust] * 8
+        print(thrusters)
+        tcm.thrusters = bytearray(thrusters)
         self.thrust_pub.publish(tcm)
 
     def run_seq(self, thrust, interval):
+        print("RUNNING SEQUENTIAL")
         tcm = FinalThrustMsg()
         for idx in range(8):
             thrusters = [127] * 8
             thrusters[idx] = thrust
+            print(thrusters)
             tcm.thrusters = bytearray(thrusters)
             time.sleep(interval)
         self.thrust_pub.publish(tcm)
 
 def main(args=None):
     rclpy.init(args=args)
-    pub = CommandPublisher()
-    sub = CommandSubscribe()
+    node = SPITest()
 
     try:
-        rclpy.spin(pub)
-        rclpy.spin(sub)
+        rclpy.spin(node)
     except KeyboardInterrupt:
         pass
 
-    pub.destroy_node()
-    sub.destroy_node()
+    node.destroy_node()
     rclpy.shutdown()
 
 if __name__ == "__main__":

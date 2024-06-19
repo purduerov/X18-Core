bcd6fbf373097cb3504f95e1af8e6b45c23e1e6a
Xavier Callait <xcallait@purdue.edu>
2024-06-19 19:35:03 +0000

    videos stuff completed

--- videos/src/videos_launch.py
+++ videos/src/videos_launch.py
@@ -5,22 +5,25 @@ from rclpy.node import Node
 import subprocess
 
 
-class PiTemp(Node):
+class Camera(Node):
     def __init__(self):
         super().__init__('cameras')
-        subprocess.run("gst-launch-1.0 -v v4l2src device=/dev/video2 ! video/x-h264, width=1920,height=1080! h264parse ! queue ! rtph264pay config-interval=10 pt=96 ! udpsink host=224.1.1.1 auto-multicast=true port=5600 sync=false buffer-size=1048576")
-        print("got here")
-    
-    def timer_callback(self):
-        subprocess.run("gst-launch-1.0 -v v4l2src device=/dev/video2 ! video/x-h264, width=1920,height=1080! h264parse ! queue ! rtph264pay config-interval=10 pt=96 ! udpsink host=224.1.1.1 auto-multicast=true port=5600 sync=false buffer-size=1048576")
+        self.declare_parameter('dev_name', 'device=/dev/video2')
+        self.declare_parameter("port_num", '5600')
+        
+        dev_name = self.get_parameter('dev_name').get_parameter_value().string_value
+        port = self.get_parameter('port_num').get_parameter_value().string_value
 
+        while True:
+            subprocess.run(["gst-launch-1.0", "-v", "v4l2src", dev_name, "!", "video/x-h264,width=1920,height=1080", "!", "h264parse", "!", "queue", "!", "rtph264pay","config-interval=10","pt=96", "!", "udpsink", "host=224.1.1.1", "auto-multicast=true", "port=" + port, "sync=false", "buffer-size=1048576"])
+    
 
 
 def main(args=None):
     rclpy.init(args = args)
-    pi_temp = PiTemp()
-    rclpy.spin(pi_temp)
-    pi_temp.destroy_node()
+    camera_node = Camera()
+    rclpy.spin(camera_node)
+    camera_node.destroy_node()
     rclpy.shutdown()
    
 if __name__ == '__main__':

f708dd65e2e062329ae9fbf6371943ac619d8935
firestrom0986 <firestorm0986@gmail.com>
2025-02-01 14:02:08 -0500

    Added topic name and timer to get_ip subscriber

--- videos/src/get_ip.py
+++ videos/src/get_ip.py
@@ -10,21 +10,38 @@ from std_msgs.msg import String
 class IpSubscriberNode(Node):
     def __init__(self):
         super().__init__('ip_subscriber_node')
-        ## TODO change the topic name to the related topic
-        self.create_subscription(String, 'TODO', self.get_ip, 10)
+        
+        self.create_subscription(String, 'surface_ip', self.get_ip, 10)
+        
 
     def get_ip(self, msg):
         received_ip = msg.data
 
         try:
             ipaddress.ipaddress(received_ip) 
-            ## TODO change the topic name to the related topic
-            self.get_logger().info(f'Received from TODO topic: "{msg.data}"')
+            
+            self.create_publisher(String, 'surface_ip', 10)
+            self.timer = self.create_timer(1.0, self.publish_stop)
+            self.stop_count = 0
+            self.stop_max_count = 5
+            self.get_logger().info(f'Received from surface_ip topic: "{msg.data}"')
+
+            ## Subprocess command
+
         except ValueError:
             self.get_logger().info(f'Invalid IP received from topic: "{msg.data}"')
 
     def launch_camera(self, ip):
         pass
+
+    def publish_stop(self):
+        msg = "STOP"
+
+        self.publisher.publish(msg)
+        self.stop_count += 1
+        if self.stop_count >= self.stop_max_count:
+            self.get_logger().info(f"Stopping publishing STOP")
+            self.timer.cancel()
 
 
 def main():

74a6fe1d1bc3406f81471ffc895999f48ab8c775
XavierJCallait <xaviercallait@gmail.com>
2025-03-10 02:53:39 +0000

    Added board enable

--- thrust_control/src/thrust_to_uart.py
+++ thrust_control/src/thrust_to_uart.py
@@ -142,6 +142,15 @@ def main(args=None):
     rclpy.init(args=args)
     node = ThrustToUARTNode()
 
+    GPIO.setwarnings(False)
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(5, GPIO.OUT, initial=GPIO.HIGH)
+
+    GPIO.output(5, GPIO.LOW)
+    time.sleep(1)
+    GPIO.output(5, GPIO.HIGH)
+
+
     try:
         rclpy.spin(node)
     except KeyboardInterrupt:

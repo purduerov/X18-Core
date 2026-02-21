1646203ff9c181a9ac9a33767f56d51e6927bcb1
TommyUbuntu <clark952@purdue.edu>
2026-02-21 13:57:13 -0500

    wtf

--- sensors/src/ADT7410.py
+++ sensors/src/ADT7410.py
@@ -13,7 +13,7 @@ class ADT7410(object):
 
     def __init__(self, bus =1): 
         try:
-            self._handle = i2c_open(bus, self._AD7410_ADDR, 0)
+            self._handle = lg.i2c_open(bus, self._AD7410_ADDR, 0)
         except:
             print("Bus %d is not available." % bus)
             print("Available busses are listed as /dev/i2c*")
@@ -27,11 +27,17 @@ class ADT7410(object):
             return False
 
         # self._bus.write_byte(self._MS5837_ADDR, self._MS5837_RESET)
-        write_byte = i2c_write_byte(self._handle, self._ADT7410_RESET)
+        write_byte = lg.i2c_write_byte(self._handle, self._ADT7410_RESET)
         # Wait for reset to complete
         sleep(0.01)
 
-        self.data = []
-        data = i2c_read_word_data(self.handle, self._ADT7410_TEMP_ADDR)
+        return True
 
-    
+    def read_temperature(self):
+        if self._handle < 0:
+            print("No bus!")
+            return False
+
+        self._temperature = lg.i2c_read_i2c_block_data(self._handle, self._ADT7410_TEMP_ADDR, 2)
+        self._temperature[1] = (self._temperature[0] << 5) | (self._temperature[1] >> 3)
+        self._temperature[0] = self._temperature[0] >> 3        

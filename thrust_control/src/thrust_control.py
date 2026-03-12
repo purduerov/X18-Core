d3452b6ae5983ee0678b32e79ce2f9f6d9d97428
Aditya Nawandhar <adityanawandhar0986@gmail.com>
2026-03-12 21:26:38 +0000

    Multiply the MULT dict with a fscotr of 5 to see if that helps with the thrust values being larger

--- thrust_control/src/thrust_control.py
+++ thrust_control/src/thrust_control.py
@@ -35,7 +35,7 @@ MEGAYEET = 3
 # [x trans, y trans, z trans, x rot, y rot, z rot]
 MULT = np.asarray([
     [1.0, 1.0, 1.0, 0.2, 0, 0.4], [1.5, 1.5, 1.5, 0.4, 0, 0.6],
-    [3, 3, 3, 0.6, 0, 1.2], [6, 6, 6, 1.2, 2.4, 2.4]
+    [3, 3, 3, 0.6, 0, 1.2], [6, 6, 6, 1.2, 0, 2.4]
 ], dtype=float)
 
 
@@ -66,7 +66,7 @@ class ThrustControlNode(Node):
         self.desired_thrusters = np.asarray([127, 127, 127, 127, 127, 127], dtype=np.int16)
         self.desired_thrusters_unramped = np.asarray([127, 127, 127, 127, 127, 127], dtype=np.int16)
 
-        self.power_mode = MEGAYEET
+        self.power_mode = MEGAYEET 
 
     def _pilot_command(self, data):
         self.desired_effort = data.desired_thrust
@@ -85,7 +85,7 @@ class ThrustControlNode(Node):
 
     def on_loop(self):
         # scale effort by multplier value
-        self.desired_effort *= MULT[self.power_mode]
+        self.desired_effort *= (MULT[self.power_mode]  * 5)
 
         # self.get_logger().info("desired_effort: " + str(self.desired_effort))
         # self.desired_effort = np.asarray([20, 20, 20, 20, 20, 20], dtype=np.int16)

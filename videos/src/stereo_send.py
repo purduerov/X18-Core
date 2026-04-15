165ada6c58b8d8c223ea798dc59ae59cc76fac39
mattman2864 <mattman2864@gmail.com>
2026-04-15 19:28:41 -0400

    added print

--- videos/src/stereo_send.py
+++ videos/src/stereo_send.py
@@ -79,43 +79,48 @@ with dai.Device(pipeline) as device:
     cv2.moveWindow("disp", 100, 100)
 
     # cv2.setMouseCallback("disp", mouse_callback)
-    while True:
-        frame_depth = q_depth.get().getFrame()
+    frame_depth = q_depth.get().getFrame()
 
-        z = frame_depth[mouse_y, mouse_x]
+    z = frame_depth[mouse_y, mouse_x]
 
-        if z > 0:
-            x_mm = (mouse_x - cx) * z / fx
-            y_mm = (mouse_y - cy) * z / fy
-            label = f"X: {int(x_mm/10)} Y: {int(y_mm/10)} Z: {int(z/10)} in cm"
-        else:
-            label = "Z: Invalid (Too close or low texture)"
-            color = (0, 0, 255) # Red for invalid
-        label += f' Length is {int(length)}'
-        # UI Overlay
-        disp_frame = cv2.normalize(frame_depth, None, 0, 255, cv2.NORM_MINMAX, cv2.CV_8U)
-        disp_frame = cv2.applyColorMap(disp_frame, cv2.COLORMAP_JET)
-        center_y, center_x = disp_frame.shape[:2]
-        
-        cv2.putText(disp_frame, label, (20, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (255, 255, 255), 2)
-        # cv2.circle(disp_frame, (mouse_x, mouse_y), 5, (255, 255, 255), -1)
+    if z > 0:
+        x_mm = (mouse_x - cx) * z / fx
+        y_mm = (mouse_y - cy) * z / fy
+        label = f"X: {int(x_mm/10)} Y: {int(y_mm/10)} Z: {int(z/10)} in cm"
+    else:
+        label = "Z: Invalid (Too close or low texture)"
+        color = (0, 0, 255) # Red for invalid
+    label += f' Length is {int(length)}'
+    # UI Overlay
+    disp_frame = cv2.normalize(frame_depth, None, 0, 255, cv2.NORM_MINMAX, cv2.CV_8U)
+    disp_frame = cv2.applyColorMap(disp_frame, cv2.COLORMAP_JET)
+    center_y, center_x = disp_frame.shape[:2]
+    
+    cv2.putText(disp_frame, label, (20, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (255, 255, 255), 2)
+    # cv2.circle(disp_frame, (mouse_x, mouse_y), 5, (255, 255, 255), -1)
 
-        if bottom != -1 and top != -1:
-          bottom_coord = (center_x // 2, bottom)
-          top_coord =  (center_x // 2, top)
+    if bottom != -1 and top != -1:
+      bottom_coord = (center_x // 2, bottom)
+      top_coord =  (center_x // 2, top)
 
-          cv2.circle(disp_frame, center=bottom_coord, radius = 1, color = (255, 255, 255), thickness = 2)
-          cv2.circle(disp_frame, center=top_coord, radius = 1, color = (255, 255, 255), thickness = 2)
-        key = cv2.waitKey(1)
-        if key == ord('q'):
-            print(f'Number of saved folders is {pic_num}')
-            break
-        elif key == ord('e'):
-          name = f'Iceberg{pic_num}'
-          output_dir = Path(name)
-          output_dir.mkdir(parents=True, exist_ok=True)
-          np.save(output_dir / 'frame_depth.npy', frame_depth)
-          np.save(output_dir / 'intrinsics.npy', intrinsics)
-          print('Thank you')
-          pic_num += 1
-        cv2.imshow("disp", disp_frame)
+      cv2.circle(disp_frame, center=bottom_coord, radius = 1, color = (255, 255, 255), thickness = 2)
+      cv2.circle(disp_frame, center=top_coord, radius = 1, color = (255, 255, 255), thickness = 2)
+    key = cv2.waitKey(1)
+    if key == ord('q'):
+        print(f'Number of saved folders is {pic_num}')
+       # break
+    elif key == ord('e'):
+      name = f'Iceberg{pic_num}'
+      output_dir = Path(name)
+      output_dir.mkdir(parents=True, exist_ok=True)
+      np.save(output_dir / 'frame_depth.npy', frame_depth)
+      np.save(output_dir / 'intrinsics.npy', intrinsics)
+      print('Thank you')
+      pic_num += 1
+    cv2.imshow("disp", disp_frame)
+
+for i in intrinsics:
+    print(i)
+
+for i in frame_depth:
+    print(i)

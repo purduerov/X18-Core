aea5559baaa5d386b8a6c3b4df0bc1f49528fb93
Aditya Nawandhar <adityanawandhar0986@gmail.com>
2026-04-22 17:48:48 -0700

    indentation

--- sensors/src/publish_sensors.py
+++ sensors/src/publish_sensors.py
@@ -10,43 +10,43 @@ import ADT7410
 import numpy as np
 
 class PublishSensors(Node):
-def __init__(self):
-    super().__init__("sensors")
-    self.depth_publisher_ = self.create_publisher(Float64, "depth", 10)
-    self.temp_publisher_ = self.create_publisher(Float32, "hat_temp", 10)
+    def __init__(self):
+        super().__init__("sensors")
+        self.depth_publisher_ = self.create_publisher(Float64, "depth", 10)
+        self.temp_publisher_ = self.create_publisher(Float32, "hat_temp", 10)
 
 
 
-    timer_period = 1.0 / 20.0 # 20 Hz
-    self.timer = self.create_timer(timer_period, self.timer_callback)
-    # try:
-    # self.sensor0 = ms5837.MS5837(1) # Initialize sensor on i2c bus 1
-    # self.sensor0.init() # Initializes with density of freshwater
-    # self.get_logger().info("Depth sensor connected")
-    # except:
-    #     self.get_logger().info("Depth sensor not found")
-    #                 self.sensor0 = None
-    #                 exit(1)
-    # self.sensor1 = ADT7410() #hat temp
-    # self.sensor1.init()
+        timer_period = 1.0 / 20.0 # 20 Hz
+        self.timer = self.create_timer(timer_period, self.timer_callback)
+        # try:
+        # self.sensor0 = ms5837.MS5837(1) # Initialize sensor on i2c bus 1
+        # self.sensor0.init() # Initializes with density of freshwater
+        # self.get_logger().info("Depth sensor connected")
+        # except:
+        #     self.get_logger().info("Depth sensor not found")
+        #                 self.sensor0 = None
+        #                 exit(1)
+        # self.sensor1 = ADT7410() #hat temp
+        # self.sensor1.init()
 
-def timer_callback(self):
-    depth_msg = Float64()
-    temp_msg = Float32()
+    def timer_callback(self):
+        depth_msg = Float64()
+        temp_msg = Float32()
 
-    #self.sensor0.read() # allows the sensor to read new data -> maybe need to add this at the begining of each call to pull new data
-    depth_msg.data = 100.0 #DUMMY DATA
-    #depth_msg.data = self.sensor0.depth()
-    self.depth_publisher_.publish(depth_msg)
-    self.get_logger().info(f"Depth: {round(depth_msg.data, 3)} km")
-    temp_msg.data = 101.9 #DUMMY DATA
-    #temp_msg.data = self.sensor1.read_temperature()
-    self.temp_publisher_.publish(temp_msg)
-    self.get_logger().info(f"Hat Temp: {round(temp_msg.data, 3)}")
+        #self.sensor0.read() # allows the sensor to read new data -> maybe need to add this at the begining of each call to pull new data
+        depth_msg.data = 100.0 #DUMMY DATA
+        #depth_msg.data = self.sensor0.depth()
+        self.depth_publisher_.publish(depth_msg)
+        self.get_logger().info(f"Depth: {round(depth_msg.data, 3)} km")
+        temp_msg.data = 101.9 #DUMMY DATA
+        #temp_msg.data = self.sensor1.read_temperature()
+        self.temp_publisher_.publish(temp_msg)
+        self.get_logger().info(f"Hat Temp: {round(temp_msg.data, 3)}")
 
-    # publishes depth in meters at a rate of 20Hz (no it doesnt)
-    # self.depth_publisher_.publish(msg)
-    # self.get_logger().info(f"Depth: {round(msg.data, 3)} km")
+        # publishes depth in meters at a rate of 20Hz (no it doesnt)
+        # self.depth_publisher_.publish(msg)
+        # self.get_logger().info(f"Depth: {round(msg.data, 3)} km")
 
 def main(args=None):
     rclpy.init(args=args)

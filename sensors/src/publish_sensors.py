810d4a7729f5d4c32bc99990b26fff7d6cca0bf7
Aditya Nawandhar <adityanawandhar0986@gmail.com>
2026-04-22 17:47:31 -0700

    characters change for encoding

--- sensors/src/publish_sensors.py
+++ sensors/src/publish_sensors.py
@@ -17,16 +17,16 @@ def __init__(self):
 
 
 
-    timer_period = 1.0 / 20.0  # 20 Hz
+    timer_period = 1.0 / 20.0 # 20 Hz
     self.timer = self.create_timer(timer_period, self.timer_callback)
     # try:
-    #     self.sensor0 = ms5837.MS5837(1)  # Initialize sensor on i2c bus 1
-    #     self.sensor0.init()  # Initializes with density of freshwater
-    #     self.get_logger().info("Depth sensor connected")
+    # self.sensor0 = ms5837.MS5837(1) # Initialize sensor on i2c bus 1
+    # self.sensor0.init() # Initializes with density of freshwater
+    # self.get_logger().info("Depth sensor connected")
     # except:
-    #     self.get_logger().info("Depth sensor not found")
-    #     self.sensor0 = None
-    #     exit(1)
+    #     self.get_logger().info("Depth sensor not found")
+    #                 self.sensor0 = None
+    #                 exit(1)
     # self.sensor1 = ADT7410() #hat temp
     # self.sensor1.init()
 
@@ -34,7 +34,7 @@ def timer_callback(self):
     depth_msg = Float64()
     temp_msg = Float32()
 
-    #self.sensor0.read()  # allows the sensor to read new data     -> maybe need to add this at the begining of each call to pull new data
+    #self.sensor0.read() # allows the sensor to read new data -> maybe need to add this at the begining of each call to pull new data
     depth_msg.data = 100.0 #DUMMY DATA
     #depth_msg.data = self.sensor0.depth()
     self.depth_publisher_.publish(depth_msg)

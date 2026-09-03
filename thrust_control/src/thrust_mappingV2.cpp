d7d81b34f62867a3f980b94ba593ef80d0137f88
TommyUbuntu <clark952@purdue.edu>
2026-09-02 20:08:12 -0400

    Created new C++ thruster file

---
+++ thrust_control/src/thrust_mappingV2.cpp
@@ -0,0 +1,15 @@
+#include <Eigen/Dense>
+#include <iostream>
+
+inline constexpr double Z_OFFSET = 0.0;
+
+class ThrustMapper:
+    public:
+        
+        ThrustMapper()
+        : invert_thrust_{1, 1, 1, -1, -1, -1},
+
+        position_vectors_{{
+
+        }}
+
